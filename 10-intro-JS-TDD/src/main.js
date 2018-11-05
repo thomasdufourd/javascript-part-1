7cb7be4d18afba7a4de7d8f8ca856b0415f64845
Thomas Dufourd <thomas.dufourd@gmail.com>
2018-11-05 22:33:34 +0100

    Adding first exercise in TDD (calc function)

---
+++ 10-intro-JS-TDD/src/main.js
@@ -0,0 +1,20 @@
+module.exports = add;
+
+function add(a, b) {
+    return null;
+}
+
+
+/*
+   Utilities functions
+ */
+
+// OBS: this is quite a ugly helper method: DELETE ME!
+function isUndefined(x) {
+    return x === undefined;
+}
+
+// Tips: DON'T use Number.NaN
+function isANumber(x) {
+    return typeof(x) === 'number';
+}
---
+++ 10-intro-JS-TDD/test/main.test.js
@@ -0,0 +1,20 @@
+const add = require('../src/main');
+
+/*
+   TODO: Make me pass! (step 1)
+ */
+test('should return 0 when empty string provided', () => {
+    expect(add('')).toBe(0);
+});
+
+/* TODO: uncomment me, run and make me pass! (step 2)
+test('should return sum when to integers are provided', () => {
+    expect(add(4, 5)).toBe(9);
+});
+*/
+
+/* TODO: uncomment me, run and make me pass! (step 3)
+test('should return the value of the only numeric parameter when the other parameter is not a number', () => {
+    expect(add("yo!", 6)).toBe(6);
+});
+*/
